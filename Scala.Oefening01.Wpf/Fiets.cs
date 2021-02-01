8db30e9f4a06521c789116d13d377c5498504ea5
jandedeuraerder-howest <jan.de.deurwaerder@howest.be>
2021-02-01 09:50:45 +0100

    Add project files.

---
+++ Scala.Oefening01.Wpf/Fiets.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scala.Oefening01.Wpf
+{
+    class Fiets
+    {
+        public string Merk { get; set; }
+        public string Serie { get; set; }
+        public decimal Verkoopprijs { get; set; }
+        public bool IsElektrisch { get; set; }
+        public byte AantalWielen { get; set; }
+
+        public Fiets()
+        {
+        }
+        public Fiets(string merk, string serie, decimal verkoopprijs, bool isElektrisch, byte aantalWielen)
+        {
+            Merk = merk;
+            Serie = serie;
+            Verkoopprijs = verkoopprijs;
+            IsElektrisch = isElektrisch;
+            AantalWielen = aantalWielen;
+        }
+        public override string ToString()
+        {
+            return $"{Merk} - {Serie}";
+        }
+    }
+}
